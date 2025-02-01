ff511cde2147ab306f56c5cfa223ad746e8f9473
lover3123 <userchat12@gmail.com>
2025-02-01 15:41:21 +0530

    commit to do list

---
+++ python/comments.py
@@ -0,0 +1,21 @@
+# this is a comment 
+#print("hello, world!")
+
+#print("hello, world!") #this is a comment 
+
+#print("hello, world!")
+#print("cheers, mate!")
+
+#this is comment 
+#written in 
+#more than one line 
+print("hello,world!")
+
+
+"""
+
+this is comment with triple upper 
+"""
+
+print("hello world!")
+
---
+++ python/variables.py
@@ -0,0 +1,11 @@
+x = 5 
+y = "john"
+print(x)
+print(y)
+
+
+x = 4         #x is a type int 
+x = "sally"   # x is now of type str 
+print(x)
+
+# this is just to show how to use the python variables 
